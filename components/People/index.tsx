7c6a844b4753d39676bc1b564c4fd468d5a7c52c
rky77 <rajudy77@gmail.com>
2025-10-19 20:14:40 +0530

    css added

--- components/People/index.tsx
+++ components/People/index.tsx
@@ -39,6 +39,7 @@ const People = () => {
 
             {/* Filter Tabs */}
             <div className="flex flex-wrap justify-start gap-1  border-t-costom pt-3 pb-4">
+              <span className="text-pink  filters">Filters </span>
               {PEOPLE_FILTER.map((filter) => (
                 <button
                   key={filter}
